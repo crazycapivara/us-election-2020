889a86c1c57d4a3feba939acc57601ec873a902e
Stefan Kuethe <crazycapivara@gmail.com>
2020-12-26 10:56:04 +0100

    Change input on click

--- shiny/us-election/app.R
+++ shiny/us-election/app.R
@@ -7,6 +7,7 @@ ui <- fluidPage(
     titlePanel("US Election"),
     sidebarLayout(
         sidebarPanel(
+            selectInput("state", "State", choices = us_states$state_name)
         ),
         mainPanel(
            leafletOutput("map")
@@ -14,17 +15,23 @@ ui <- fluidPage(
     )
 )
 
-server <- function(input, output) {
-    output$map <- renderLeaflet({#
+server <- function(input, output, session) {
+    output$map <- renderLeaflet({
         leaflet() %>%
             addTiles() %>%
             addPolygons(
                 data = us_states,
                 weight = 1,
                 color = "white",
-                fillColor = "blue",
-                popup = ~ sprintf("%s (%i)", state_name, number_of_votes)
+                fillColor = "blue"
+                , layerId = ~ state_name
+                #, popup = ~ sprintf("%s (%i)", state_name, number_of_votes)
             )
+    })
+    
+    observeEvent(input$map_shape_click, {
+        print(input$map_shape_click)
+        updateSelectInput(session, "state", selected = input$map_shape_click$id)
     })
 }
 
